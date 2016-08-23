889b324a1d62aabf71817485ef45d4fb5286544d
Gaylord Mazelier <gaylord.mazelier@mfglabs.com>
2016-08-23 18:40:12 +0200

    Call `/load` endpoint with a random token from a CSV feed

--- gatling/src/test/scala/psugdemo/DemoSimulation.scala
+++ gatling/src/test/scala/psugdemo/DemoSimulation.scala
@@ -9,6 +9,8 @@ class DemoSimulation extends Simulation {
 
   val httpConf = http.baseURL("http://127.0.0.1:9000")
 
+  val tokens = csv("tokens.csv").random
+
   val statusScenario = scenario("Status")
     .forever(
       pace(5 seconds)
@@ -19,9 +21,24 @@ class DemoSimulation extends Simulation {
         )
     )
 
+  val loadScenario = scenario("Load")
+    .feed(tokens)
+    .forever(
+      pace(10 seconds)
+        .exec(
+          http("load")
+            .post("/load")
+            .queryParam("token", "${token}")
+            .check(status is 200)
+        )
+    )
+
   setUp(
     statusScenario.inject(
       atOnceUsers(1)
+    ),
+    loadScenario.inject(
+      rampUsers(5) over (10 seconds)
     )
   ).protocols(httpConf)
     .maxDuration(5 minutes)
